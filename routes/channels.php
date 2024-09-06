a34643a810f3ca228516dde5a57b392dd2d500da
kenberu-dev <kenberu.development@gmail.com>
2024-09-06 15:13:11 +0900

    チャット機能に合わせて編集

--- routes/channels.php
+++ routes/channels.php
@@ -1,7 +1,8 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Support\Facades\Broadcast;
 
-Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
-    return (int) $user->id === (int) $id;
-});
+// Broadcast::channel('message.meetinglog.{meetinglogId}', function(User $user, int $meetinglogId) {
+//     return ;
+// });

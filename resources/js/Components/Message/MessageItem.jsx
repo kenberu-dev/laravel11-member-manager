98de7989cb2761cdfd46703a888c1362617525de
kenberu <kenberu.development@gmail.com>
2024-09-09 16:15:47 +0900

    メッセージ表示部分のコンポーネントの作成と実装

---
+++ resources/js/Components/Message/MessageItem.jsx
@@ -0,0 +1,47 @@
+import { formatMessageDateLong } from "@/helpers";
+import { usePage } from "@inertiajs/react";
+import UserAvatar from "./UserAvatar";
+
+
+
+
+const MessageItem = ({ message }) => {
+    const currnetUser = usePage().props.auth.user;
+
+    return (
+        <div
+            className={
+                "chat " +
+                (message.sender.id === currnetUser.id 
+                    ? "chat-end" 
+                    : "chat-start")
+            }
+        >
+            {<UserAvatar user={message.sender} />}
+            <div className="chat-header">
+                {message.sender.id !== currnetUser.id
+                    ? message.sender.name
+                    : ""}
+                <time className="text-xs opacity-50 ml-2">
+                    {formatMessageDateLong(message.created_at)}
+                </time>
+            </div>
+            <div
+                className={
+                    "chat-bubble relative " +
+                    (message.sender.id === currnetUser.id 
+                        ? "chat-bubble-info" 
+                        : "")
+                }
+            >
+                <div className="chat-message">
+                    <div className="chat-message-content">
+                        {message.message}
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default MessageItem;

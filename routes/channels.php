6710ac07ede4b6fb8e04f76c68cc6c18839506cc
kenberu <kenberu.development@gmail.com>
2024-09-09 16:20:23 +0900

    面談記録内のチャット機能のチャネルを作成し実装した

--- routes/channels.php
+++ routes/channels.php
@@ -3,6 +3,6 @@
 use App\Models\User;
 use Illuminate\Support\Facades\Broadcast;
 
-// Broadcast::channel('message.meetinglog.{meetinglogId}', function(User $user, int $meetinglogId) {
-//     return ;
-// });
+Broadcast::channel('message.meetinglog.{meetinglogId}', function(User $user) {
+    return $user;
+});

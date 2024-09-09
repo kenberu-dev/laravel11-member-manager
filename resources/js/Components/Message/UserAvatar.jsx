06a3c7bc82bdbb06ff9c937be5ae8b1b4e2ed109
kenberu <kenberu.development@gmail.com>
2024-09-09 16:16:35 +0900

    職員のアバター画像表示コンポーネントの作成と実装

---
+++ resources/js/Components/Message/UserAvatar.jsx
@@ -0,0 +1,28 @@
+const UserAvatar = ({ user, profile = false }) => {
+    const sizeClass = profile ? "w-40" : "w-8";
+
+    return (
+        <>
+            {user.avatar && (
+                <div className={`chat-image avatar`}>
+                    <div className={`rounded-full ${sizeClass}`}>
+                        <img src={user.avatar} />
+                    </div>
+                </div>
+            )}
+            {!user.avatar && (
+                <div className={`chat-image avatar placeholder`}>
+                    <div 
+                        className={`rounded-full ${sizeClass} bg-gray-200 text-gray-400 text-center`}
+                    >
+                        <span className="text-xl">
+                            {user.name.substring(0, 1)}
+                        </span>
+                    </div>
+                </div>
+            )}
+        </>
+    )
+}
+
+export default UserAvatar;

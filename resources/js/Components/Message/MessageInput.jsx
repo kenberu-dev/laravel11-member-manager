9328a80ad71c036e68e270d080580c6bdc6afd45
kenberu-dev <kenberu.development@gmail.com>
2024-09-10 15:37:24 +0900

    メッセージ送信時にDB二内容を保存できる機能を追加

--- resources/js/Components/Message/MessageInput.jsx
+++ resources/js/Components/Message/MessageInput.jsx
@@ -1,12 +1,44 @@
 import { PaperAirplaneIcon } from "@heroicons/react/24/solid";
 import NewMessageInput from "./NewMessageInput";
 import { useState } from "react";
+import axios from "axios";
 
 
-const MessageInput = () => {
+const MessageInput = (meetingLogId = null) => {
     const [newMessage, setNewMessage] = useState("");
     const [inputErrorMessage, setInputErrorMessage] = useState("");
     const [messageSending, setMessageSending] = useState(false);
+
+    // meetingLogIdがオブジェクト型だったので配列型に直している
+    const id = Object.values(meetingLogId)
+
+    const onSendClick = () => {
+      if (newMessage.trim() === "") {
+        setInputErrorMessage("メッセージを入力してください");
+        setTimeout(() => {
+          setInputErrorMessage("");
+        }, 3000);
+        return;
+      }
+      const formData = new FormData();
+      formData.append("message", newMessage);
+      formData.append("meeting_logs_id", id[0]);
+
+      setMessageSending(true);
+      axios.post(route("message.store"), formData, {
+        onUploadProgress: (progressEvent) => {
+          const progress = Math.round(
+            (progressEvent.loaded / progressEvent.total) * 100
+          );
+          console.log("progress", progress);
+        }
+      }).then((response) => {
+        setNewMessage("");
+        setMessageSending(false);
+      }).catch((error) => {
+        setMessageSending(false);
+      });
+    }
 
     return (
         <div className="flex flex-wrap items-start border-t border-slate-700 py-3">
@@ -14,9 +46,13 @@ const MessageInput = () => {
                 <div className="flex ">
                     <NewMessageInput
                         value={newMessage}
+                        onSend={onSendClick}
                         onChange={(ev) => setNewMessage(ev.target.value)}
                     />
-                    <button className="btn btn-info rounded-l-none">
+                    <button
+                      onClick={onSendClick}
+                      className="btn btn-info rounded-l-none"
+                    >
                         {messageSending && (
                             <span className="loading loading-spinner loading-xs"></span>
                         )}

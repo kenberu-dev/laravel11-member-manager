98ba520b7133484bf8e34e226b7f9da627da64e1
kenberu-dev <kenberu.development@gmail.com>
2024-09-12 14:14:34 +0900

    チャンネル許可設定

--- routes/channels.php
+++ routes/channels.php
@@ -2,7 +2,8 @@
 
 use App\Models\User;
 use Illuminate\Support\Facades\Broadcast;
+use Illuminate\Support\Facades\Log;
 
-Broadcast::channel('message.meetinglog.{meetinglogId}', function(User $user) {
-    return $user;
+Broadcast::channel('message.meetinglog.{meetinglogId}', function(User $user, int $meetinglogId) {
+    return $user ? $user : null;
 });

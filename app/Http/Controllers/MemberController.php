2a4162bbda1ec1e206f54dd45875cf7ea4b199dc
kenberu-dev <kenberu.development@gmail.com>
2024-09-17 13:57:50 +0900

    利用者の新規登録・編集・削除機能の実装

--- app/Http/Controllers/MemberController.php
+++ app/Http/Controllers/MemberController.php
@@ -67,7 +67,11 @@ class MemberController extends Controller
      */
     public function create()
     {
-        //
+        $offices = Office::all();
+
+        return inertia("Member/Create", [
+            "offices" => OfficeResource::collection($offices),
+        ]);
     }
 
     /**
@@ -75,7 +79,10 @@ class MemberController extends Controller
      */
     public function store(StoreMemberRequest $request)
     {
-        //
+        $data = $request->validated();
+        Member::create($data);
+
+        return to_route('member.index');
     }
 
     /**
@@ -105,9 +112,10 @@ class MemberController extends Controller
         }
 
         $meetingLogs = $query->orderBy($sortField, $sortDirection)->paginate(10);
-        $users = User::select("users.*")
+        $users = User::select("users.id", "users.name")
                     ->leftJoin("meeting_logs", "meeting_logs.user_id", "=", "users.id")
                     ->where("meeting_logs.member_id", "=", $member->id)
+                    ->groupBy("users.id", "users.name")
                     ->get();
 
         $queryParams = request()->query();
@@ -125,7 +133,12 @@ class MemberController extends Controller
      */
     public function edit(Member $member)
     {
-        //
+        $offices = Office::all();
+
+        return inertia("Member/Edit", [
+            'member' => new MemberResource($member),
+            'offices' => OfficeResource::collection($offices),
+        ]);
     }
 
     /**
@@ -133,7 +146,10 @@ class MemberController extends Controller
      */
     public function update(UpdateMemberRequest $request, Member $member)
     {
-        //
+        $data = $request->validated();
+        $member->update($data);
+
+        return to_route('member.index');
     }
 
     /**
@@ -141,6 +157,7 @@ class MemberController extends Controller
      */
     public function destroy(Member $member)
     {
-        //
+        $member->delete();
+        return to_route('member.index');
     }
 }
--- app/Http/Requests/StoreMemberRequest.php
+++ app/Http/Requests/StoreMemberRequest.php
@@ -11,7 +11,7 @@ class StoreMemberRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,12 @@ class StoreMemberRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            "name" => "required|string",
+            "sex" => "required",
+            "office_id" => "required",
+            "status" => "required",
+            "characteristics" => "nullable|string",
+            "notes" => "nullable|string",
         ];
     }
 }

adc31b5f62ed3688ba0e4b7bc2da5fb7e677bcf9
kenberu <kenberu.development@gmail.com>
2024-09-19 22:22:24 +0900

    更新処理用のリクエストの実装

--- app/Http/Requests/UpdateOfficeRequest.php
+++ app/Http/Requests/UpdateOfficeRequest.php
@@ -11,7 +11,7 @@ class UpdateOfficeRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,10 @@ class UpdateOfficeRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            "name" => ["required", "string"],
+            "zip_code" => ["required", "string", "regex:/^[0-9-]+$/"],
+            "address" => ["required", "string"],
+            "phone_number" => ["required", "string", "regex:/^[0-9-]+$/"],
         ];
     }
 }

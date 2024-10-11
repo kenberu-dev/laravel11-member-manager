f4e0aeeefd8353b024ff61a9db3ef67c0d50911f
kenberu-dev <kenberu.development@gmail.com>
2024-10-11 10:35:08 +0900

    面談記録テーブルとのリレーションの不備を修正

--- app/Models/User.php
+++ app/Models/User.php
@@ -55,4 +55,9 @@ class User extends Authenticatable
     {
         return $this->belongsTo(Office::class);
     }
+
+    public function meetingLogs()
+    {
+        return $this->hasMany(MeetingLog::class);
+    }
 }

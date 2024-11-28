c44fed771a1a8ed8d4d936171e4d98d467769e44
kenberu-dev <kenberu.development@gmail.com>
2024-11-28 11:29:52 +0900

    外部対応のファクトリ実装

---
+++ database/factories/ExternalMessageFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Message>
+ */
+class MessageFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'message' => fake()->realText(100),
+            'sender_id' => fake()->randomElement(\App\Models\User::pluck('id')->toArray()),
+            'external_meeting_logs_id' => fake()->randomElement(\App\Models\ExternalMeetingLog::pluck('id')->toArray()),
+            'created_at' =>fake()->dateTimeBetween('-1 year', 'now'),
+        ];
+    }
+}

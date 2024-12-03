de04f084ddcd4d285361038e9dc8f354c6228cff
kenberu-dev <kenberu.development@gmail.com>
2024-12-03 10:54:49 +0900

    クラス名の修正

--- database/factories/ExternalMessageFactory.php
+++ database/factories/ExternalMessageFactory.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Message>
  */
-class MessageFactory extends Factory
+class ExternalMessageFactory extends Factory
 {
     /**
      * Define the model's default state.
